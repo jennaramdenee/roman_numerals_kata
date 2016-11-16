b7b4b9846e8f315e32ea682cdd66606ee629a91e
Jenna Ramdenee <jenna_ramdenee@hotmail.co.uk>
2016-11-16 10:09:11 +0000

    Attempt in progress

--- lib/roman_numerals.rb
+++ lib/roman_numerals.rb
@@ -1,36 +1,73 @@
-def convert_to_number(letter)
-  return 1 if letter == "I"
-  return 5 if letter == "V"
-  return 10 if letter == "X"
-  return 50 if letter == "L"
-  return 100 if letter == "C"
-  return 500 if letter == "D"
-  return 1000 if letter == "M"
-  return "Not a valid letter"
-end
+# def convert_to_number(letter)
+#   return 1 if letter == "I"
+#   return 5 if letter == "V"
+#   return 10 if letter == "X"
+#   return 50 if letter == "L"
+#   return 100 if letter == "C"
+#   return 500 if letter == "D"
+#   return 1000 if letter == "M"
+#   return "Not a valid letter"
+# end
 
-def convert_to_roman_numerals(number)
-  return "I" if number == 1
-  return "V" if number == 5
-  return "X" if number == 10
-  return "L" if number == 50
-  return "C" if number == 100
-  return "D" if number == 500
-  return "M" if number == 1000
-  return "Not a valid number"
-end
+convert_to_roman_numerals = {
+  1 => "I",
+  5 => "V",
+  10 => "X",
+  50 => "L",
+  100 => "C",
+  500 => "D",
+  1000 => "M"
+}
+
+divisors = []
 
 def divisible_by?(number, divisor)
   number % divisor == 0
 end
 
 def find_next_divisor(number)
-  return 1000 if divisible_by?(number, 1000)
-  return 500 if divisible_by?(number, 500)
-  return 100 if divisible_by?(number, 100)
-  return 50 if divisible_by?(number, 50)
-  return 10 if divisible_by?(number, 10)
-  return 5 if divisible_by?(number, 5)
-  return 1 if divisible_by?(number, 1)
+  if divisible_by?(number, 1000)
+    add_divisor_to_array(1000)
+    subtract(number, 1000)
+  elsif divisible_by?(number, 500)
+    add_divisor_to_array(500)
+    subtract(number, 500)
+  elsif divisible_by?(number, 100)
+    add_divisor_to_array(100)
+    subtract(number, 100)
+  elsif divisible_by?(number, 50)
+    add_divisor_to_array(50)
+    subtract(number, 50)
+  elsif divisible_by?(number, 10)
+    add_divisor_to_array(10)
+    subtract(number, 10)
+  elsif divisible_by?(number, 5)
+    add_divisor_to_array(5)
+    subtract(number, 5)
+  elsif divisible_by?(number, 1)
+    add_divisor_to_array(1)
+    subtract(number, 1)
+  end
+end
 
+def subtract(number, number2)
+  number = number - number2
+  if !final_number?
+    find_next_divisor(number)
+  else
+    add_divisor_to_array(number)
+    change_to_roman_numerals(divisors)
+  end
+end
+
+def final_number?
+  convert_to_roman_numerals.has_key?(number)
+end
+
+def add_divisor_to_array(divisor)
+  divisors << divisor
+end
+
+def change_to_roman_numerals(divisors)
+  divisors.inject { |sum, divisor|, sum + convert_to_roman_numerals[divisor] }
 end
--- spec/roman_numerals_spec.rb
+++ spec/roman_numerals_spec.rb
@@ -2,77 +2,77 @@ require './lib/roman_numerals'
 
 describe 'roman_numerals' do
 
-  context 'Conversion from roman numeral to numbers' do
+  # context 'Conversion from roman numeral to numbers' do
+  #
+  #   it 'returns 1 when passed "I"' do
+  #     expect(convert_to_number("I")).to eq 1
+  #   end
+  #
+  #   it 'returns 5 when passed "V"' do
+  #     expect(convert_to_number("V")).to eq 5
+  #   end
+  #
+  #   it 'returns 10 when passed "X"' do
+  #     expect(convert_to_number("X")).to eq 10
+  #   end
+  #
+  #   it 'returns 50 when passed "L"' do
+  #     expect(convert_to_number("L")).to eq 50
+  #   end
+  #
+  #   it 'returns 100 when passed "C"' do
+  #     expect(convert_to_number("C")).to eq 100
+  #   end
+  #
+  #   it 'returns 500 when passed "D"' do
+  #     expect(convert_to_number("D")).to eq 500
+  #   end
+  #
+  #   it 'returns 1000 when passed "M"' do
+  #     expect(convert_to_number("M")).to eq 1000
+  #   end
+  #
+  #   it 'returns "Not a valid letter" when passed "Q"' do
+  #     expect(convert_to_number("Q")).to eq "Not a valid letter"
+  #   end
+  #
+  # end
 
-    it 'returns 1 when passed "I"' do
-      expect(convert_to_number("I")).to eq 1
-    end
-
-    it 'returns 5 when passed "V"' do
-      expect(convert_to_number("V")).to eq 5
-    end
-
-    it 'returns 10 when passed "X"' do
-      expect(convert_to_number("X")).to eq 10
-    end
-
-    it 'returns 50 when passed "L"' do
-      expect(convert_to_number("L")).to eq 50
-    end
-
-    it 'returns 100 when passed "C"' do
-      expect(convert_to_number("C")).to eq 100
-    end
-
-    it 'returns 500 when passed "D"' do
-      expect(convert_to_number("D")).to eq 500
-    end
-
-    it 'returns 1000 when passed "M"' do
-      expect(convert_to_number("M")).to eq 1000
-    end
-
-    it 'returns "Not a valid letter" when passed "Q"' do
-      expect(convert_to_number("Q")).to eq "Not a valid letter"
-    end
-
-  end
-
-  context 'Conversion from numbers to roman numerals' do
-
-    it 'returns "I" when passed 1' do
-      expect(convert_to_roman_numerals(1)).to eq "I"
-    end
-
-    it 'returns "V" when passed 5' do
-      expect(convert_to_roman_numerals(5)).to eq "V"
-    end
-
-    it 'returns "X" when passed 10' do
-      expect(convert_to_roman_numerals(10)).to eq "X"
-    end
-
-    it 'returns "L" when passed 50' do
-      expect(convert_to_roman_numerals(50)).to eq "L"
-    end
-
-    it 'returns "C" when passed 100' do
-      expect(convert_to_roman_numerals(100)).to eq "C"
-    end
-
-    it 'returns "D" when passed 500' do
-      expect(convert_to_roman_numerals(500)).to eq "D"
-    end
-
-    it 'returns "M" when passed 1000' do
-      expect(convert_to_roman_numerals(1000)).to eq "M"
-    end
-
-    it 'returns "Not a valid number" when passed "66"' do
-      expect(convert_to_roman_numerals(66)). to eq "Not a valid number"
-    end
-
-  end
+  # context 'Conversion from numbers to roman numerals' do
+  #
+  #   it 'returns "I" when passed 1' do
+  #     expect(convert_to_rn(1)).to eq "I"
+  #   end
+  #
+  #   it 'returns "V" when passed 5' do
+  #     expect(convert_to_roman_numerals[5]).to eq "V"
+  #   end
+  #
+  #   it 'returns "X" when passed 10' do
+  #     expect(convert_to_roman_numerals(10)).to eq "X"
+  #   end
+  #
+  #   it 'returns "L" when passed 50' do
+  #     expect(convert_to_roman_numerals(50)).to eq "L"
+  #   end
+  #
+  #   it 'returns "C" when passed 100' do
+  #     expect(convert_to_roman_numerals(100)).to eq "C"
+  #   end
+  #
+  #   it 'returns "D" when passed 500' do
+  #     expect(convert_to_roman_numerals(500)).to eq "D"
+  #   end
+  #
+  #   it 'returns "M" when passed 1000' do
+  #     expect(convert_to_roman_numerals(1000)).to eq "M"
+  #   end
+  #
+  #   it 'returns "Not a valid number" when passed "66"' do
+  #     expect(convert_to_roman_numerals(66)). to eq "Not a valid number"
+  #   end
+  #
+  # end
 
   context 'Divisibility' do
 
@@ -132,6 +132,20 @@ describe 'roman_numerals' do
 
   end
 
+  context 'Subtract' do
 
+    it 'can subtract one number from another' do
+      expect(subtract(10, 1)).to eq 9
+    end
 
+  end
+
+  context 'Add divisors to array' do
+
+    it 'adds divisors to array' do
+      add_divisors_to_array(50)
+      expect(divisors).to include(50)
+    end
+
+  end
 end
